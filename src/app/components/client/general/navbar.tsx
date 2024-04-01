07d5cb2a8594eb8a4bdbcb528d39836d56814b5b
Oladunni Faith <dunni82@yahoo.com>
2024-04-01 15:30:55 -0700

    FIX: minor changes to landing website (#3)
    * FIX: minor changes to landing website
    
    * chore: refactor landing page components and navbar
    
    * FIX: nav review changes request
    
    ---------
    
    Co-authored-by: Theophilus <isahtheophilus@gmail.com>

--- src/app/components/client/general/navbar.tsx
+++ src/app/components/client/general/navbar.tsx
@@ -1,15 +1,18 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useRef, useState, useEffect } from "react";
 
 type NavState = boolean;
 
 const Navbar: React.FC = () => {
+  const router = useRouter();
   const [openNav, setOpenNav] = useState<NavState>(false);
   const hamburgerRef = useRef<HTMLDivElement>(null);
 
-  const handleNavOpen = (e: React.MouseEvent): void => {
+  const handleNavOpen = (): void => {
     setOpenNav(!openNav);
   };
 
@@ -30,19 +33,45 @@ const Navbar: React.FC = () => {
     };
   }, [hamburgerRef]);
 
+  // if mobile menu is open and user switched to desktop view, close the mobile menu
+  useEffect(() => {
+    const handleResize = (): void => {
+      if (window.innerWidth > 768) {
+        setOpenNav(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const MobileMenu = () => {
     return (
-      <div className="flex flex-col w-3/4 fixed top-20 right-2 z-10 h-1/4 py-2 px-6 bg-black-700">
-        <button className="md:block bg-black-600 border border-black-700 hover:bg-black-500 text-[16px] leading-3 text-white-100 font-normal-200 py-[16px] my-3 px-8 rounded">
-          <Link href={"/resources"}>Bitcoin Education</Link>
+      <div className="flex flex-col w-3/4 fixed top-20 right-2 z-10 h-1/4 py-2 px-6 gap-y-5 bg-black-700 items-center">
+        
+        <Link href={"/resources"} className="hover:text-orange-100 text-white-100 my-4">Bitcoin Education</Link>
+        
+
+        <button
+          onClick={() => {
+            window.open("http://app.stealth.money/account/login", "_blank");
+          }}
+        >
+          <span className="text-white-100 bg-black-600 border border-black-700 hover:bg-black-500 leading-3 py-[16px] my-3 px-8 rounded">
+            Log in
+          </span>
         </button>
-        <button className="md:block bg-black-600 border border-black-700 hover:bg-black-500 text-[16px] leading-3 text-white-100 font-normal-200 py-[16px] my-3 px-8 rounded">
-          <Link href={"http://app.stealth.money/account/login"}>Log in</Link>
-        </button>
-        <button className="md:block bg-orange-100 border border-black-700 hover:bg-orange-500 text-[16px] leading-3 text-white-100 font-normal-200 px-8 my-3 py-[16px] rounded">
-          <Link href={"http://app.stealth.money/account/register"}>
-            Get Started
-          </Link>
+
+        <button
+          onClick={() => {
+            window.open("http://app.stealth.money/account/register", "_blank");
+          }}
+          className="md:block bg-orange-100 border border-black-700 hover:bg-orange-500 text-[16px] leading-3 text-white-100 font-normal-200 px-8 my-3 py-[16px] rounded"
+        >
+          <span>Get Started</span>
         </button>
       </div>
     );
@@ -51,7 +80,12 @@ const Navbar: React.FC = () => {
   return (
     <nav className="max-h-[100px] border-b border-b-black-500">
       <div className="flex justify-between items-center py-2 px-4 md:px-12">
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => {
+            router.push("/");
+          }}
+        >
           <Image
             className="w-[80px] h-[50px] md:w-[132px]"
             src="/stealth-logo.svg"
@@ -61,7 +95,7 @@ const Navbar: React.FC = () => {
             priority
           />
         </div>
-        <div className="buttons flex space-x-4">
+        <div className="buttons flex space-x-4 items-center">
           <div
             className="flex flex-col items-center gap-y-1 md:hidden"
             onClick={handleNavOpen}
@@ -71,18 +105,28 @@ const Navbar: React.FC = () => {
             <span className="bg-white-100 w-[20px] h-[2px]"></span>
             <span className="bg-white-100 w-[20px] h-[2px]"></span>
           </div>
-          <button className="hidden md:block border border-black-700 hover:bg-black-500 text-[16px] leading-3 text-white-100 font-normal-200 py-[16px] px-8 rounded">
-            <Link href={"/resources"}>
-              Bitcoin Education
-            </Link>
+
+          <Link href={"/resources"} className="hover:text-orange-100 text-white-100 hidden md:block">Bitcoin Education</Link>
+
+          <button
+            onClick={() => {
+              window.open("http://app.stealth.money/account/login", "_blank");
+            }}
+            className="hidden cursor-pointer md:block border border-black-700 hover:bg-black-600 bg-black-500 text-[16px] leading-3 text-white-100 font-normal-200 py-[16px] px-8 rounded"
+          >
+            <span className="hidden md:block">Log in</span>
           </button>
-          <button className="hidden md:block bg-black-600 border border-black-700 hover:bg-black-500 text-[16px] leading-3 text-white-100 font-normal-200 py-[16px] px-8 rounded">
-            <Link href={"http://app.stealth.money/account/login"}>Log in</Link>
-          </button>
-          <button className="hidden md:block bg-orange-100 border border-black-700 hover:bg-orange-500 text-[16px] leading-3 text-white-100 font-normal-200 px-8 py-[16px] rounded">
-            <Link href={"http://app.stealth.money/account/register"}>
-              Get Started
-            </Link>
+
+          <button
+            onClick={() => {
+              window.open(
+                "http://app.stealth.money/account/register",
+                "_blank"
+              );
+            }}
+            className="hidden cursor-pointer md:block bg-orange-100 border border-black-700 hover:bg-orange-500 text-[16px] leading-3 text-white-100 font-normal-200 px-8 py-[16px] rounded"
+          >
+            <span>Get Started</span>
           </button>
 
           {openNav && MobileMenu()}
